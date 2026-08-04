dff4bc0174bdb2f3104628e4466a5cd8af01f777
KuuKangBest <waaxiiwee@mail.ustc.edu.cn>
2026-08-04 11:36:51 +0800

    Delay game start one frame to ensure test blocks exist first

--- Assets/_Scripts/Core/Game/GameManager.cs
+++ Assets/_Scripts/Core/Game/GameManager.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Events;
@@ -54,6 +55,12 @@ namespace TDTTetris.Core
         private void Start()
         {
             ValidateReferences();
+            StartCoroutine(DelayedStart());
+        }
+
+        private System.Collections.IEnumerator DelayedStart()
+        {
+            yield return null; // 等一帧，确保 TestSceneBuilder 完成
             StartGame();
         }
 
